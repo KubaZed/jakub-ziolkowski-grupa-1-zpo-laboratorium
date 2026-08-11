d123409c70fbe987972d2c8bbb2518aaa81f9557
KubaZed <kziolkowski@3s.com.pl>
2026-08-11 19:02:58 +0200

    Add files via upload

---
+++ projekty/lab-2/WalidatorZadanieDodatkowe/src/com/validation/strategy/EmailStrategy.java
@@ -0,0 +1,24 @@
+package com.validation.strategy;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
+import com.validation.annotation.Email;
+import com.validation.annotation.ValidationFor;
+
+@ValidationFor(Email.class)
+public class EmailStrategy implements ValidationStrategy {
+
+    @Override
+    public Optional<String> validate(Field field, Object value) {
+        if (field.isAnnotationPresent(Email.class) && value != null && value instanceof String) {
+            Email annotation = field.getAnnotation(Email.class);
+            String email = (String) value;
+
+            if (!email.matches("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")) {
+                String errorInfo = String.format("Pole %s: %s", field.getName(), annotation.message());
+                return Optional.of(errorInfo);
+            }
+        }
+        return Optional.empty();
+    }
+}
